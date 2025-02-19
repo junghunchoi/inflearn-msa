450ec5c633f625f862d1847e35dcc0fec84234df
junghunchoi <azzudi@naver.com>
2025-02-19 18:52:02 +0900

    init

---
+++ src/main/java/org/example/inflearnmsa/InflearnMsaApplication.java
@@ -0,0 +1,13 @@
+package org.example.inflearnmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/inflearnmsa/InflearnMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.inflearnmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
